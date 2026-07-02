977927cd51d8544734e4d1ec85bd1a206f73e224
rhoitjadhav <jadhavrohit497@gmail.com>
2026-07-02 20:25:11 +0530

    feat: add provider URLs to config with env override support

--- src/config.py
+++ src/config.py
@@ -7,3 +7,10 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "sqlite:///ip_lookup.db"
     # f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
 )
+
+
+# Providers config
+AWS_URL = os.getenv("AWS_URL", "https://ip-ranges.amazonaws.com/ip-ranges.json")
+GCP_URL = os.getenv("GCP_URL", "https://www.gstatic.com/ipranges/cloud.json")
+AZURE_URL = os.getenv("AZURE_URL", "https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_{date}.json")
+CLOUDFLARE_URL = os.getenv("CLOUDFLARE_URL", "https://www.cloudflare.com/ips-v4")
